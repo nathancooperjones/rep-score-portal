eab24b33cd92848ac86b209184b860cb6bd86412
nathancooperjones <nathancooperjones@gmail.com>
2022-06-07 14:09:34 -0500

    no subdomain for now

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(marspetcare.repscoreportal.org www.marspetcare.repscoreportal.org)
+domains=(repscoreportal.org www.repscoreportal.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="nate@therepproject.org" # Adding a valid address is strongly recommended
